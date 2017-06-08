8d874b96308b4a8bc86d2173066a0db95f8ec69b
ksmai <kasingmai@gmail.com>
2017-06-08 15:15:16 +0800

    Implement ActionComponent

--- src/app/action/action.component.ts
+++ src/app/action/action.component.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
+import { ActionData } from '../../models';
+import { DataService } from '../core/data.service';
 
 @Component({
   selector: 'ads-action',
   templateUrl: './action.component.html',
   styleUrls: ['./action.component.scss'],
 })
-export class ActionComponent {
+export class ActionComponent implements OnInit {
+  actionData: Observable<ActionData>;
+
+  constructor(private dataService: DataService) {
+  }
+
+  ngOnInit(): void {
+    this.actionData = this.dataService.getActionData();
+  }
 }
