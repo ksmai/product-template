55d749f3612f5f425c6b814509edd6b992f9c099
ksmai <kasingmai@gmail.com>
2017-06-08 13:58:53 +0800

    Implement GalleryComponent

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
+import { GalleryData } from '../../models';
+import { DataService } from '../core/data.service';
 
 @Component({
   selector: 'ads-gallery',
   templateUrl: './gallery.component.html',
   styleUrls: ['./gallery.component.scss'],
 })
-export class GalleryComponent {
+export class GalleryComponent implements OnInit {
+  galleryData: Observable<GalleryData>;
+
+  constructor(private dataService: DataService) {
+  }
+
+  ngOnInit(): void {
+    this.galleryData = this.dataService.getGalleryData();
+  }
 }
